8f552dfc9c2c11b035c231e22c00b5442a9ca271
code-with-naimish <naimish028@gmail.com>
2025-07-04 22:54:39 +0530

    ui progress

--- src/components/main-layout/side-nav.tsx
+++ src/components/main-layout/side-nav.tsx
@@ -22,7 +22,22 @@ const SideNav = () => {
             </NavLink>
             :
             <>
-              Children</>
+              <span className="!pl-4 nav-link border-transparent !text-sm opacity-60 ">{val.label}</span>
+
+              {val.children.map((val2, i2) => {
+                return <NavLink key={i2}
+                  to={{
+                    pathname: val2?.path
+                  }}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active-nav-link " : "nav-link inactive-nav-link "
+                  }
+                >
+                  {val2?.label}
+                </NavLink>
+              })}
+
+            </>
           }
 
 
