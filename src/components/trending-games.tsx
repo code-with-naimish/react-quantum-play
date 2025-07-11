716de8875095924346215b3e34c909de620d5990
code-with-naimish <naimish028@gmail.com>
2025-07-12 02:09:15 +0530

    progress

---
+++ src/components/trending-games.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+import type { HomeFilterEnum } from "../_enum/home.filter.enum";
+import type { GameModel } from "../_models/game.model"
+import GameCard from "./game-card"
+
+const TrendinGames = (props: {
+  items: GameModel[],
+  title: HomeFilterEnum,
+  navLink: string
+
+}) => {
+  if (!props?.items || props?.items?.length === 0) return null;
+  return (
+    <section className="group/section">
+      <div className="flex mb-8 md:items-center md:justify-between md:flex-row flex-col gap-4">
+        <h2 className="left-line  text-2xl pl-4 leading-none font-semibold">Trending {props?.title}</h2>
+        <Link className="group-hover/section:opacity-100 opacity-0 text-fuchsia-500 font-medium hover:text-fuchsia-600 transition-all duration-300" to={props?.navLink}>View All</Link>
+      </div>
+      <div className="grid xl:grid-cols-5  lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6">
+        {props?.items?.map((val, i) => {
+          return <div key={i}>
+            <GameCard val={val} />
+          </div>
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default TrendinGames
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,8 @@ import type { GameModel } from "../_models/game.model";
 import { fetchOptions } from "../_helpers/fetch-option";
 import { toast } from "react-toastify";
 import { HomeFilterEnum } from "../_enum/home.filter.enum";
+import TrendinGames from "../components/trending-games";
+import { Link } from "react-router";
 
 const filterGenre = (arr: GameModel[], genre: HomeFilterEnum) => {
   return arr?.filter((val) => {
@@ -65,15 +67,40 @@ const Home = () => {
 
 
   return (
-    <section>
-      <div className="hero-bg   p-12 overflow-hidden rounded-2xl  ">
-        <div className="max-w-lg">
-          <h1 className="md:text-5xl text-4xl font-semibold mb-4 ">Next-Gen Gaming, Zero Cost</h1>
-          <p className="text-lg font-light mb-8 md:max-w-2xl">Experience cutting-edge graphics, intense gameplay, and endless fun — all without spending a dime.</p>
-          <PrimaryBtn title="Explore now" />
+    <>
+      <section className="mb-10">
+        <div className="hero-bg   p-12 overflow-hidden rounded-2xl  ">
+          <div className="max-w-lg">
+            <h1 className="md:text-5xl text-4xl font-semibold mb-4 ">Next-Gen Gaming, Zero Cost</h1>
+            <p className="text-lg font-light mb-8 md:max-w-2xl">Experience cutting-edge graphics, intense gameplay, and endless fun — all without spending a dime.</p>
+            <Link to="/sort-by/popularity">  <PrimaryBtn title="Explore now" /></Link>
+
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      {loading && <div className="no-data">
+        <div className="loader"></div>
+      </div>}
+      {!loading && (!items || items?.length === 0) && <div className=" no-data">
+        <p>No games available!</p>
+      </div>}
+      {!loading && (items && items?.length > 0) &&
+        <div className="space-y-10">
+          <TrendinGames title={HomeFilterEnum.MMORPG} items={mmorpg} navLink="/category/mmorpg" />
+          <TrendinGames title={HomeFilterEnum.SHOOTER} items={shooter} navLink="/category/shooter" />
+          <TrendinGames title={HomeFilterEnum.STRATEGY} items={strategy} navLink="/category/strategy" />
+          <TrendinGames title={HomeFilterEnum.BATTLE_ROYALE} items={battleRoyal} navLink="/category/battle-royale" />
+          <TrendinGames title={HomeFilterEnum.SPORTS} items={sports} navLink="/category/sports" />
+          <TrendinGames title={HomeFilterEnum.CARD_GAME} items={cardGame} navLink="/category/card" />
+        </div>
+      }
+
+
+
+
+    </>
+
 
   )
 }
