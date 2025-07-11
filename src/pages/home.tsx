dd069947c9ab781f6e3df6133dbcfad1b44afa4d
code-with-naimish <naimish028@gmail.com>
2025-07-11 15:42:07 +0530

    progress

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,8 +5,8 @@ const Home = () => {
 
   return (
     <section>
-      <div className="hero-bg   py-18 px-12 overflow-hidden rounded-2xl flex flex-col  justify-center items-center ">
-        <div>
+      <div className="hero-bg   p-12 overflow-hidden rounded-2xl  ">
+        <div className="max-w-lg">
           <h1 className="md:text-5xl text-4xl font-semibold mb-4 ">Next-Gen Gaming, Zero Cost</h1>
           <p className="text-lg font-light mb-8 md:max-w-2xl">Experience cutting-edge graphics, intense gameplay, and endless fun — all without spending a dime.</p>
           <PrimaryBtn title="Explore now" />
