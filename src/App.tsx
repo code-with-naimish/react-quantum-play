34f44ae0d52f87ce8632149588b7169f8aac15b8
code-with-naimish <naimish028@gmail.com>
2025-07-15 01:44:17 +0530

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,38 @@ import './App.css'
 import Logo from './components/main-layout/logo'
 import SideNav from './components/main-layout/side-nav'
 import RootNavs from './components/root-navs'
+import { useState } from 'react'
 
 const App = () => {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
+
+  const toggleAside = () => {
+    setShowSidebar(prev => !prev)
+  }
+
+  const closeAside = () => {
+    setShowSidebar(false)
+  }
+
   return (
     <div className='grid md:grid-cols-12 grid-cols-1   overflow-hidden  bg-black text-white h-screen'>
 
-      <aside className='md:col-span-2 flex flex-col overflow-hidden border-r border-purple-500/20'>
+      <aside className={`md:col-span-2 flex flex-col overflow-hidden border-r border-purple-500/20
+      md:h-auto h-screen md:overflow-y-hidden overflow-y-auto
+         bg-black  z-50 md:static fixed md:bg-transparent md:z-auto transition-all
+       ${showSidebar ? "-translate-x-full   " : "md:translate-x-0"}
+        `}>
         <Logo />
 
 
-        <SideNav />
+        <div className=' flex-1 overflow-y-auto overflow-x-hidden'>
+          <SideNav />
+        </div>
       </aside>
 
       <div className='md:col-span-10 flex flex-col overflow-hidden'>
-        <header className='border-b  border-purple-500/20 px-6 py-3 flex justify-between items-center gap-6'>
+        <header onClick={toggleAside} className='border-b  border-purple-500/20 px-6 py-3 flex justify-between items-center gap-6'>
           <h1 className=' text-xl gradient-text font-semibold'>Welcome</h1>
           <a href="https://github.com/code-with-naimish" target='_blank' className='flex items-center flex-none  text-xl justify-center w-10 h-10 rounded-full  border border-purple-500/20'>
             <FaGithub /></a>
--- src/components/main-layout/side-nav.tsx
+++ src/components/main-layout/side-nav.tsx
@@ -5,47 +5,45 @@ import { navLinks } from "../../_helpers/nav-link"
 
 const SideNav = () => {
   return (
-    <div className=' flex-1 overflow-y-auto overflow-x-hidden'>
-      <nav className="px-4">
-        {navLinks.map((val, i) => {
-          return <div key={i}>
+    <nav className="px-4">
+      {navLinks.map((val, i) => {
+        return <div key={i}>
 
-            {!val.children ?
-              <NavLink
-                to={{
-                  pathname: val?.path
-                }}
-                className={({ isActive }) =>
-                  isActive ? "nav-link active-nav-link" : "nav-link inactive-nav-link"
-                }
-              >
-                {val?.label}
-              </NavLink>
-              :
-              <>
-                <span className="!pl-2 nav-link border-transparent !text-sm opacity-60 ">{val.label}</span>
+          {!val.children ?
+            <NavLink
+              to={{
+                pathname: val?.path
+              }}
+              className={({ isActive }) =>
+                isActive ? "nav-link active-nav-link" : "nav-link inactive-nav-link"
+              }
+            >
+              {val?.label}
+            </NavLink>
+            :
+            <>
+              <span className="!pl-2 nav-link border-transparent !text-sm opacity-60 ">{val.label}</span>
 
-                {val.children.map((val2, i2) => {
-                  return <NavLink key={i2}
-                    to={{
-                      pathname: val2?.path
-                    }}
-                    className={({ isActive }) =>
-                      isActive ? "nav-link active-nav-link " : "nav-link inactive-nav-link "
-                    }
-                  >
-                    {val2?.label}
-                  </NavLink>
-                })}
+              {val.children.map((val2, i2) => {
+                return <NavLink key={i2}
+                  to={{
+                    pathname: val2?.path
+                  }}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active-nav-link " : "nav-link inactive-nav-link "
+                  }
+                >
+                  {val2?.label}
+                </NavLink>
+              })}
 
-              </>
-            }
+            </>
+          }
 
 
-          </div>
-        })}
-      </nav>
-    </div>
+        </div>
+      })}
+    </nav>
   )
 }
 
